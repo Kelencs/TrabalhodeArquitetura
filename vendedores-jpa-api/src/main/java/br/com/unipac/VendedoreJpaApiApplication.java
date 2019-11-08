4735694c1650e5b510665b3681121df3e1082f2a
Kelencs <50461309+Kelencs@users.noreply.github.com>
2019-11-08 20:44:18 -0300

    Add files via upload

---
+++ vendedores-jpa-api/src/main/java/br/com/unipac/VendedoreJpaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.unipac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendedoreJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendedoreJpaApiApplication.class, args);
+	}
+
+}
---
+++ vendedores-jpa-api/src/test/java/br/com/unipac/VendedoresJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unipac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendedoresJpaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
